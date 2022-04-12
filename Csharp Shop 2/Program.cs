6f13946d20d8c9bf20ef946228658ec6d5f65097
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-12 15:04:10 +0200

    Metodo riempi svuota stampa e bevi completati, classi prodotto e acqua inserite e testate

--- Csharp Shop 2/Program.cs
+++ Csharp Shop 2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Csharp_Shop_2;
+
+Acqua miaAcqua = new Acqua("Nepi", "Acqua leggermente frizzante", 1.5, 7, "Nepi" , 3.00 , 22);
+
+miaAcqua.StampaDati();
+miaAcqua.Bevi(1);
+miaAcqua.Riempi(0.5);
