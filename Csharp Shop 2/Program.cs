46fc755b341fd3bc4de4fcbc566b04c92e5a082e
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-12 15:49:19 +0200

    classe cibo in scatola creata con metodo mangia e apri, implementate negli oggetti

--- Csharp Shop 2/Program.cs
+++ Csharp Shop 2/Program.cs
@@ -5,3 +5,10 @@ Acqua miaAcqua = new Acqua("Nepi", "Acqua leggermente frizzante", 1.5, 7, "Nepi"
 miaAcqua.StampaDati();
 miaAcqua.Bevi(1);
 miaAcqua.Riempi(0.5);
+Acqua.DaLitriAGalloni("2");
+
+Prodotto mioProdotto = new Prodotto("Pane", "Pane ai cereali");
+mioProdotto.StampaDati();
+
+CiboInScatola mioCibo = new CiboInScatola("Scatoletta di tonno", "Tonno al naturale", 300);
+mioCibo.Mangia(200);
